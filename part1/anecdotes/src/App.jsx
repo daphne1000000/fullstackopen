93716db063c912570c2966e9d5132eeaff4f0e4c
Daphne Jarjoura <daphnejarjoura@gmail.com>
2024-11-04 23:38:28 -0500

    Finished exercise 1.12

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 
+const Button = ({handleClick, text}) => (
+ <button onClick = {handleClick}>
+  {text}
+ </button>
+)
+
 const App = () => {
   const anecdotes = [
-   'If it hurst, do it more often.',
-   'Adding manpower to a late software porject makes it later!',
+   'If it hurts, do it more often.',
+   'Adding manpower to a late software project makes it later!',
    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
    'Premature optimization is the root of all evil.',
@@ -11,12 +17,18 @@ const App = () => {
    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
    'The only way to go fast, is to go well.'
    ]
-
+   console.log(anecdotes.length)
    const [selected, setSelected] = useState(0)
-
-  return (
+   
+   const handleClick = () => {
+     setSelected(Math.floor(Math.random() * anecdotes.length))
+   }
+  
+   return (
    <div>
     {anecdotes[selected]}
+    <br />
+    <Button handleClick = {handleClick} text='next anecdote' />
    </div>
   )
 }
